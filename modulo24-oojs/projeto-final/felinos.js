7e55b20148e24c1b1e874443487c033b3f8cc379
Bruno Coitinho <bruno.pereira1988@gmail.com>
2025-10-29 09:16:01 -0300

    feat: modulo 24 - Projeto final adicionado

---
+++ modulo24-oojs/projeto-final/felinos.js
@@ -0,0 +1,62 @@
+function Felino(tamanho, peso){
+    let _tamanho = tamanho;
+    let _peso = peso;
+
+    this.getTamanho = function(){
+        return _tamanho;
+    }
+
+    this.getPeso = function(){
+        return _peso;
+    }
+
+    this.setTamanho = function(tamanho){
+        if(tamanho > 0){
+            _tamanho = tamanho;
+        }
+        else{
+            _tamanho = 0;
+        }
+    }
+
+    this.setPeso = function(peso){
+        if(peso > 0){
+            _peso = peso;
+        }
+        else{
+            _peso = 0;
+        }
+    }
+
+    this.emitirSom = function(){
+        console.log("Barulho do felino")
+    }
+
+}
+
+function Gato(tamanho, peso){
+
+    felino.call(this, tamanho, peso);
+
+    this.emitirSom = function(){
+        console.log("Miau")
+    }
+}
+
+function Leao(tamanho, peso){
+
+    felino.call(this, tamanho, peso);
+
+    this.emitirSom = function(){
+        console.log("Roar")
+    }
+}
+
+
+const gatinho1 = new Gato(0.5, 2)
+const gatinho2 = new Gato(0.4, 2.1)
+const leao = new Leao(2, 190)
+
+gatinho1.emitirSom()
+gatinho2.emitirSom()
+leao.emitirSom()
